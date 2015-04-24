9a908eef9f1f97b9ddf6bc00b51815cbc5527d21
Geoff Winner <Geoff@Geoffs-MacBook-Pro.local>
2015-04-24 11:05:15 -0700

    add more styling and add a who is here page

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var studentRoster = angular.module('studentRoster', []);
+var studentRoster = angular.module('studentRoster', ['ui.router']);
+studentRoster.config(function($stateProvider) {
+
+
+$stateProvider.state('home', {
+  url: "",
+  templateUrl: "partials/home.html"
+});
+
+$stateProvider.state('whoishere', {
+  url:"/whoishere",
+  templateUrl: "partials/whoishere.html"
+});
+
+});
